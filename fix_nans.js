df776b173e2e45022adc82743d3653e7b965fcab
Alexander Kondrashkin <alkondr@yandex-team.ru>
2017-10-21 21:54:03 +0300

    another fix with negative numbers

--- fix_nans.js
+++ fix_nans.js
@@ -3,8 +3,8 @@ const fs = require('fs');
 const db = require('./product_base');
 
 const fixed_products = db.products.map(el => {
-    const new_price = !!el.price ? el.price : Math.random() * 500;
-    const new_actual = el.actualPrice != "NaN" ? el.actualPrice : new_price - (Math.random() * new_price / 2.0).toFixed(2);
+    const new_price = !!el.price ? el.price : (Math.random() * 500).toFixed(2);
+    const new_actual = el.actualPrice != "NaN" ? el.actualPrice : (new_price - Math.random() * new_price / 2.0).toFixed(2);
 
     return {
         ...el,
